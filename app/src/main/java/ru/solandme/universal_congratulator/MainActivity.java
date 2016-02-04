a39c0eb71738ac995f932eb2c058c2d72ae224f5
Solomin Andrey <solomin.andrey@gmail.com>
2016-02-04 21:04:49 +0800

    Добавил перевод на русский и добавил скроллинг содержимого активити

--- app/src/main/java/ru/solandme/universal_congratulator/MainActivity.java
+++ app/src/main/java/ru/solandme/universal_congratulator/MainActivity.java
@@ -2,6 +2,8 @@ package ru.solandme.universal_congratulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickNewYear(View view){
+        Toast.makeText(this, "Click to New Year", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onClickValentine(View view){
+        Toast.makeText(this, "Click to Valentine", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
