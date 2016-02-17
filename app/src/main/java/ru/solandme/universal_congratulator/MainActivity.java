00f6986d7caa7d57fc43c5ecc430b1c101496e92
Solomin Andrey <solomin.andrey@gmail.com>
2016-02-17 11:22:01 +0800

    Добавил заглушку для главного меню

--- app/src/main/java/ru/solandme/universal_congratulator/MainActivity.java
+++ app/src/main/java/ru/solandme/universal_congratulator/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -19,6 +22,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_settings :
+                //TODO добавить настройки приложения
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about_app:
+                //TODO добавить описание приложения
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initViews() {
