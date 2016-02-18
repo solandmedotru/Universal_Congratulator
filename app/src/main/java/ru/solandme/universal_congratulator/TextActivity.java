da4784e110507b40b58286828048e8502f6a13f5
Solomin Andrey <solomin.andrey@gmail.com>
2016-02-19 01:58:06 +0800

    Добавил floating action button для календаря

--- app/src/main/java/ru/solandme/universal_congratulator/TextActivity.java
+++ app/src/main/java/ru/solandme/universal_congratulator/TextActivity.java
@@ -2,11 +2,14 @@ package ru.solandme.universal_congratulator;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
 
 import java.util.Random;
 
